1dec7c6b3fedf63f3282949a1f14ae22339f2db9
shijieming <jie8309541@qq.com>
2016-11-07 22:20:55 +0800

    111

---
+++ src/main/java/com/sjmcrf/yangfan/entity/Info.java
@@ -0,0 +1,95 @@
+package com.sjmcrf.yangfan.entity;
+
+public class Info {
+	public int id;
+	
+	private String mobile;
+	
+	private String addr;
+	
+	private String qq1;
+	
+	private String qq2;
+	
+	private String email;
+	
+	private String wechat;
+	
+	private String map;
+	
+	private int deleted;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getQq1() {
+		return qq1;
+	}
+
+	public void setQq1(String qq1) {
+		this.qq1 = qq1;
+	}
+
+	public String getQq2() {
+		return qq2;
+	}
+
+	public void setQq2(String qq2) {
+		this.qq2 = qq2;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getWechat() {
+		return wechat;
+	}
+
+	public void setWechat(String wechat) {
+		this.wechat = wechat;
+	}
+
+	public String getMap() {
+		return map;
+	}
+
+	public void setMap(String map) {
+		this.map = map;
+	}
+
+	public int getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(int deleted) {
+		this.deleted = deleted;
+	}
+	
+	
+}
